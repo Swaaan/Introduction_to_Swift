dd64a266204f88c4ba10ba011a1499749147ef91
白鳥 友里恵 <shiratori_yurie@cyberagent.co.jp>
2015-12-25 11:27:41 +0900

    lesson1 map kit

---
+++ map/map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by 白鳥 友里恵 on 2015/12/25.
+//  Copyright (c) 2015年 swan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
